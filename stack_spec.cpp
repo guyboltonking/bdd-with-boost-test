f257712feeccd4ff81d2646866e12d06f14c727c
Guy Bolton King <guy@waftex.com>
2013-04-10 15:18:09 +0100

    Added SpecLogFormatter

--- stack_spec.cpp
+++ stack_spec.cpp
@@ -1,6 +1,40 @@
-#define BOOST_TEST_MAIN
+#define BOOST_TEST_ALTERNATIVE_INIT_API
 #include <boost/test/included/unit_test.hpp>
+#include <boost/algorithm/string.hpp>
 
+//==============================================================================
+
+class SpecLogFormatter: 
+  public boost::unit_test::output::compiler_log_formatter {
+
+public:
+  SpecLogFormatter(): m_indent(0) {}
+
+private:
+  void test_unit_start(std::ostream &os, 
+    boost::unit_test::test_unit const& tu) 
+  {
+    os << std::string(m_indent, ' ') << 
+      boost::replace_all_copy(tu.p_name.get(), "_", " ") << std::endl;
+    m_indent += 2;
+  }
+
+  void test_unit_finish(std::ostream &os,
+    boost::unit_test::test_unit const& tu, unsigned long elapsed)
+  {
+    m_indent -= 2;
+  }
+
+  int m_indent;
+};
+
+//==============================================================================
+
+bool init_unit_test()
+{
+  boost::unit_test::unit_test_log.set_formatter(new SpecLogFormatter);
+  return true;
+}
 
 //==============================================================================
 
