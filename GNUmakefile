6ccf06d645c9edb908d06e92948a051d22e0fe3c
Guy Bolton King <guy@waftex.com>
2013-04-12 19:19:15 +0100

    Removed irrelevant code

--- GNUmakefile
+++ GNUmakefile
@@ -1,8 +1,5 @@
 all: run-stack-spec-catch
 
-run-boost-range: boost-range
-	./boost-range
-
 run-stack-spec: stack_spec
 	./stack_spec --log_level=test_suite
 
@@ -14,6 +11,3 @@ run-stack-spec-catch: stack_spec_catch
 
 stack_spec_catch: stack_spec_catch.cpp
 	clang++ -std=c++0x -I. -Iboost_1_53_0 -o stack_spec_catch stack_spec_catch.cpp
-
-boost-range: boost-range.cpp
-	clang++ -std=c++0x -Iboost_1_53_0 -o boost-range boost-range.cpp
--- stack_spec_catch.cpp
+++ stack_spec_catch.cpp
@@ -28,7 +28,7 @@ private:
 
 //==============================================================================
 
-TEST_CASE("a stack", "[stack]") {
+SCENARIO("a stack", "[stack]") {
   Stack<int> stack;
 
   SECTION("when initialised") {
@@ -57,58 +57,3 @@ TEST_CASE("a stack", "[stack]") {
   }
 
 }
-
-#if 0
-struct a_stack_ {
-  Stack<int> stack;
-};
-
-BOOST_FIXTURE_TEST_SUITE(a_stack, a_stack_)
-  BOOST_AUTO_TEST_SUITE(when_initialised)
-    BOOST_AUTO_TEST_CASE(should_be_empty)
-    {
-      BOOST_CHECK(stack.empty());
-    }
-  BOOST_AUTO_TEST_SUITE_END()
-
-  BOOST_AUTO_TEST_SUITE(pop)
-
-    struct on_an_empty_stack_: a_stack_ {
-      on_an_empty_stack_() {
-        BOOST_REQUIRE(stack.empty());
-      }
-    };
-
-    BOOST_FIXTURE_TEST_SUITE(on_an_empty_stack, on_an_empty_stack_)
-      BOOST_AUTO_TEST_CASE(should_have_no_effect)
-      {
-        stack.pop();
-        BOOST_CHECK(stack.empty());
-      }
-    BOOST_AUTO_TEST_SUITE_END()
-
-    struct on_a_stack_with_a_single_member_: a_stack_ {
-      on_a_stack_with_a_single_member_() {
-        stack.push(1);
-        BOOST_REQUIRE_EQUAL(stack.size(), 1);
-      }
-    };
-
-    BOOST_FIXTURE_TEST_SUITE(on_a_stack_with_a_single_member, 
-                             on_a_stack_with_a_single_member_)
-      BOOST_AUTO_TEST_CASE(should_result_in_an_empty_stack)
-      {
-        stack.pop();
-        BOOST_CHECK(stack.empty());
-      }
-
-      BOOST_AUTO_TEST_CASE(should_reduce_the_stack_size_by_one)
-      {
-        std::size_t orig_size = stack.size();
-        stack.pop();
-        BOOST_CHECK_EQUAL(stack.size(), orig_size - 1);
-      }
-    BOOST_AUTO_TEST_SUITE_END()
-  BOOST_AUTO_TEST_SUITE_END()
-BOOST_AUTO_TEST_SUITE_END()
-#endif
